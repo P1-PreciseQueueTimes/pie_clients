d33f3ac37dfb0b1e0efda1cc641a8b1b18e4623e
AFitBear <asbjornholmpedersen@gmail.com>
2024-11-20 13:10:05 +0100

    updated_code

--- ip_finder.py
+++ ip_finder.py
@@ -1,12 +1,11 @@
 #This module finds the ip link thing. your wlcome, yours truly, Aborn.
 import subprocess
+def find_ip():
+    out = subprocess.check_output(["ip","link"])
+    txt = str(out)
 
-out = subprocess.check_output(["ip","link"])
+    start_index = txt.index("wlx00")
+    end_index = txt.index(":",start_index)
 
-txt = str(out)
-
-start_index = txt.index("wlx00")
-
-end_index = txt.index(":",start_index)
-
-print(txt[start_index:end_index])
+    #print(txt[start_index:end_index])
+    return txt[start_index:end_index]
--- receiver.py
+++ receiver.py
@@ -3,14 +3,17 @@ import time
 import os
 import requests
 import socket
+import ip_finder
 
-wifi_interface = "interface"
+
+wifi_interface = ip_finder.find_ip()
 sender_mac = "dc:a6:32:54:ac:c5"
 channel = 13
 user = ""
 url = "https://washington-removed-explore-designer.trycloudflare.com/post/testing/receiver"
 
 old_mac = ""
+old_time = 0
 
 host_name = socket.gethostname() 
 
@@ -19,27 +22,32 @@ capture = pyshark.LiveCapture(interface=wifi_interface)
 
 
 def print_info(packet):
-    global old_mac
-    
-    if not packet["WLAN_MGT"] or not packet["WLAN"] or not packet["WLAN_RADIO"]:
-        return
-    if not packet["WLAN"].ta:
-        return
-    
-    if packet["WLAN"].fc_type_subtype == "0x0004":
-        if old_mac == packet["WLAN"].ta:
+    global old_mac, old_time
+    current_time = time.time_ns()
+    try:
+        if not packet["WLAN.MGT"] or not packet["WLAN"] or not packet["WLAN_RADIO"]:
             return
+        if not packet["WLAN"].ta:
+            return
+        
+        if packet["WLAN"].fc_type_subtype == "0x0004":
+            if old_mac == packet["WLAN"].ta or (current_time - old_time) / 1000000.0 < 5000.0:
+                return
 
-        if packet["WLAN"].ta == sender_mac:
-            current_time = time.time_ns()
+            if packet["WLAN"].ta == sender_mac: 
+                
 
-            out_obj = {"host_name":host_name,"internal_time":current_time}
+                out_obj = {"host_name":host_name,"internal_time":current_time}
 
-            requests.post(url,json=out_obj)
+                requests.post(url,json=out_obj)
 
-        old_mac = packet["WLAN"].ta
+                old_time = current_time
+
+            old_mac = packet["WLAN"].ta
+    except Exception :
+        pass
 
 out_str = f'airmon-ng start "{wifi_interface}" {channel} >/dev/null 2>&1'
 os.system("echo %s|sudo -S %s" % (user, out_str))
 
-capture.apply_on_packets(print_info, packet_count=100)
+capture.apply_on_packets(print_info, packet_count=10000)#, timeout=50)

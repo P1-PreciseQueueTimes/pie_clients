5cd831984dbee1cceab1005d4534d20d0fce6993
AFitBear <asbjornholmpedersen@gmail.com>
2024-11-20 12:13:40 +0100

    hello

---
+++ ip_finder.py
@@ -0,0 +1,12 @@
+#This module finds the ip link thing. your wlcome, yours truly, Aborn.
+import subprocess
+
+out = subprocess.check_output(["ip","link"])
+
+txt = str(out)
+
+start_index = txt.index("wlx00")
+
+end_index = txt.index(":",start_index)
+
+print(txt[start_index:end_index])
